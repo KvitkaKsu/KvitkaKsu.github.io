6169ae27e35c8a1ae4ea49c52ceb64f2b3cd7948
Kseny <mamsic1369@gmail.com>
2018-08-20 14:11:41 +0300

    js and new correction

--- js/slider.js
+++ js/slider.js
@@ -1,2 +1,32 @@
-var name = 'Ксюша';
-console.log(name);
+    
+    /*mob-menu*/
+
+    function openbox(id) {
+        if (document.getElementById(id).style.display == "none")
+           {document.getElementById(id).style.display = "block"}
+        else
+           {document.getElementById(id).style.display = "none"}
+        }
+
+    function closebox(id) {
+        if (document.getElementById(id).style.display == "none")
+            {document.getElementById(id).style.display = "block"}
+        else
+            {document.getElementById(id).style.display = "none"}
+        }
+
+        /*mob-menu*/
+
+        /*team*/
+
+        function openteam(id) {
+            if (document.getElementById(id).style.display == "none")
+               {document.getElementById(id).style.display = "block"}
+            else
+               {document.getElementById(id).style.display = "none"}
+            }
+    
+        
+
+        /*team*/
+        
